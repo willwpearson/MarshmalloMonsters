d29c28794fff72c671dce14a5d329d9b9c648c38
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-15 13:32:56 -0600

    Added code that lets you interact with the amount of eyes and arms you want to eat off the monster.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,7 @@
 package monster.controller;
 
 import monster.model.MarshmalloMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
@@ -13,5 +14,31 @@ public class MonsterController
 		System.out.println("I am feeling hungry, I am going to eat one of Guillermo's arms.");
 		guillermo.setArmCount(guillermo.getArmCount() - 1);
 		System.out.println(guillermo);
+		
+		interactWithMonster(guillermo);
+		armEat(guillermo);
+	}
+	
+	private void interactWithMonster(MarshmalloMonster currentMonster)
+	{
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println(currentMonster.getName() + " wants to know how many eyes you want eat, please type in how many");
+		int consumed = myScanner.nextInt();
+		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
+		System.out.println(currentMonster);
+		
+		myScanner.close();
+		
+	}
+	
+	private void armEat(MarshmalloMonster myMonster)
+	{
+		Scanner armScanner = new Scanner(System.in);
+		System.out.println(myMonster.getName() + " wants to know how many arms you want to eat, please type in how many");
+		int eaten = armScanner.nextInt();
+		myMonster.setArmCount(myMonster.getArmCount() - eaten);
+		System.out.println(myMonster);
+		
+		armScanner.close();
 	}
 }
