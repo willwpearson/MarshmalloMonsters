2b25ebc8698a5f68956c14425f9112e20bd50c67
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-15 12:11:34 -0600

    Created a toString method.

--- src/monster/model/MarshmalloMonster.java
+++ src/monster/model/MarshmalloMonster.java
@@ -2,5 +2,33 @@ package monster.model;
 
 public class MarshmalloMonster
 {
-
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	public MarshmalloMonster()
+	{
+		//initializes everything to 0, false or null.
+	}
+	
+	public MarshmalloMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	{
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.tentacleAmount = tentacleCount;
+		this.hasBloop = hasBloop;
+	}
+	
+	public String toString()
+	{
+		String description = "Yo soy un monstruo, me llamo es " + name + ", yo tengo " + eyeCount + " ojos y " ;
+		description += tentacleAmount + " 'tentacles', y yo tengo " + armCount + " brazos!. " + hasBloop + " habla tu ";
+		description += " si tengo bloop.";
+		return description;
+	}
 }
