b3dd73cf25294021f295758e35c9705003accd02
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-19 12:58:18 -0600

    Added two more if and else statements to choose tentacle and eye amount.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -21,12 +21,28 @@ public class MonsterController
 	private void interactWithMonster(MarshmalloMonster currentMonster)
 	{
 		Scanner myScanner = new Scanner(System.in);
-		System.out.println(currentMonster.getName() + " wants to know how many eyes you want eat, please type in how many");
-		int consumed = myScanner.nextInt();
-		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
+		System.out.println(currentMonster.getName() + " wants to know how many tentacles you want eat, please type in how many");
+		double consumed = myScanner.nextDouble();
+		if (consumed == 0)
+		{
+			System.out.println("You don't want any???");
+		}
+		else if (consumed < 0)
+		{
+			System.out.println("You can't do that...");
+		}
+		else if (consumed > currentMonster.getTentacleAmount())
+		{
+			System.out.println("It doesn't have that many...");
+		}
+		else
+		{
+		currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumed);
 		System.out.println(currentMonster);
+		}
 		
-		System.out.println("How many arms are you interested in eating?, It has " + currentMonster.getArmCount());
+		
+		System.out.println("How many arms are you interested in eating? It has " + currentMonster.getArmCount());
 		int armEat = myScanner.nextInt();
 		
 		if(armEat == 0)
@@ -39,12 +55,33 @@ public class MonsterController
 		}
 		else if(armEat > currentMonster.getArmCount())
 		{
-			System.out.println("You are not allowed to eat more than exist on me xD ");
+			System.out.println("You are not allowed to eat more than exist on him xD ");
 		}
 		else
 		{
 			currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
 			System.out.println("OK, now it has this many arms: " + currentMonster.getArmCount());
+		}
+		
+		System.out.println("How many eyes are you interested in eating? It has " + currentMonster.getEyeCount());
+		int eyeEat = myScanner.nextInt();
+		
+		if(eyeEat == 0)
+		{
+			System.out.println("Really not hungry???" );
+		}
+		else if(eyeEat < 0)
+		{
+			System.out.println("Come on, you should know better by now");
+		}
+		else if(eyeEat > currentMonster.getEyeCount())
+		{
+			System.out.println("I thought you would have this by now...");
+		}
+		else
+		{
+			currentMonster.setEyeCount(currentMonster.getEyeCount() - eyeEat);
+			System.out.println("OK, now it has this many eyes: " + currentMonster.getEyeCount());
 		}
 		
 		
